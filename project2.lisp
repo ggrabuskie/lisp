8838d76b6517e5520b814908a8e1136fdaeff86f
ggrabuskie <ggrabuskie@csu.fullerton.edu>
2018-11-10 19:09:31 -0800

    WORKING MAKE_EXPR()

--- project2.lisp
+++ project2.lisp
@@ -7,11 +7,13 @@
     ((= rand_num 0) (setq v 'x))
     ((= rand_num 1) (setq v 'y))
     ((= rand_num 2) (setq v 'z))))
-(defun getvori ()
-  (setq rand_num (random 2))
+
+(defun getk ()
+  (setq rand_num (random 3))
   (cond
     ((= rand_num 0) (setq vori (geti)))
-    ((= rand_num 1) (setq vori (getv)))))
+    ((= rand_num 1) (setq vori (getv)))
+    ((= rand_num 2) (setq vori (make_expr)))))
 
 (defun geto ()
   (setq rand_num (random 3))
@@ -20,10 +22,12 @@
     ((= rand_num 0) (setq op '*))
     ((= rand_num 1) (setq op '+))
     ((= rand_num 2) (setq op '-))))
+
 (defun make_expr ()
   (Setq expr (list (geto)))
   (print expr)
   (dotimes (n (random 4))
-    (print n)
-    (setq expr (append expr (list (geti))))
-    (print expr)))
+    (setq expr (append expr (list (getk)))))
+    (return-from make_expr expr))
+    
+
