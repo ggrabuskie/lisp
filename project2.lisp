7ae60d46cd7fa7052c74a3e791a6aafe15e690f6
Georden Grabuskie <ggrabuskie@csu.fullerton.edu>
2018-11-11 17:37:07 -0800

    added evaluate

--- project2.lisp
+++ project2.lisp
@@ -1,6 +1,7 @@
-(defvar *x* 1)
-(defvar *y* 2)
-(defvar *z* 3)
+(defvar x 1)
+(defvar y 2)
+(defvar z 3)
+
 (defun geti ()
   (setq const_int (- (random 19) 9)))
 
@@ -21,7 +22,6 @@
 (defun geto ()
   (setq rand_num (random 3))
   (cond
-
     ((= rand_num 0) (setq op '*))
     ((= rand_num 1) (setq op '+))
     ((= rand_num 2) (setq op '-))))
@@ -54,6 +54,14 @@
     ((not (listp rt)) 0)
     (t (let ((cc (length rt)))
          (+ cc (apply #'+ (mapcar #'cell-count rt)))))))
+
 (defun not-list (mlist)
 "return true if not a list"
- (not (listp mlist)))
+  (not (listp mlist)))
+
+(defun evaluate(expr)
+  "Evaluate the expression"
+  (setq X xvalue)
+  (setq Y yvalue)
+  (setq Z zvalue)
+  (setq result (eval expr)))
